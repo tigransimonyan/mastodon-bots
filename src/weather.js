f72a7b0bf4586a30098c8cd2ec6f8120a5b57ae1
tigran <tigran.contact@gmail.com>
2023-01-30 16:13:53 +0400

    update

--- src/weather.js
+++ src/weather.js
@@ -1,40 +1,27 @@
-const megalodon = require("megalodon");
+const megalodon = require('megalodon');
 const generator = megalodon.default;
-const https = require("https");
+const axios = require('axios').default;
 
-const BASE_URL = "https://xn--y9a6bah4ck.xn--y9a3aq";
+const BASE_URL = 'https://xn--y9a6bah4ck.xn--y9a3aq';
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-const client = generator("mastodon", BASE_URL, ACCESS_TOKEN);
-const locations = ["Երեւան", "Լոռի", "Սեւան", "Դիլիջան", "Արցախ"];
+const client = generator('mastodon', BASE_URL, ACCESS_TOKEN);
+const locations = ['Երեւան', 'Լոռի', 'Սեւան', 'Դիլիջան', 'Արցախ'];
 
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const post = (array) => {
-  const status = `Եղանակը Հայաստանում \n\n${array.join(
-    "\n\n"
-  )}`;
-  client
-    .postStatus(status)
-    .then(() => console.log("Done!"))
-    .catch((error) => console.log(error));
+  const status = `Եղանակը Հայաստանում\n\n${array.join('\n\n')}`;
+  return client.postStatus(status);
 };
 
-const wttrs = [];
 locations.forEach((location) => {
   const path = `/${location}?lang=hy&format=%c+${location}ում %t է (զգալի՝ %f)։`;
-  https
-    .get("https://wttr.in".concat(encodeURI(path)), (response) => {
-      let str = "";
-      response.on("data", (chunk) => {
-        str += chunk;
-      });
-      response.on("end", () => {
-        wttrs.push(str);
-        if (wttrs.length === locations.length) {
-          post(wttrs);
-        }
-      });
-    })
-    .end();
+  axios
+    .get('https://wttr.in'.concat(encodeURI(path)))
+    .then((response) => post(response.data))
+    .then(() => console.log('Done!'))
+    .catch((error) => {
+      console.log(error);
+    });
 });
